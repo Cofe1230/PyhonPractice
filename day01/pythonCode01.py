042824267cce2f06ade47d8e31841521c22e0c5c
Cofe1230 <winner1230@gmail.com>
2023-08-31 11:06:20 +0900

    파이썬 기본 +글자쓰괴

---
+++ day01/pythonCode01.py
@@ -0,0 +1,14 @@
+import os
+
+a=int(input())
+b=int(input())
+result=a+b
+print(a, "+", b, "=", result)
+result = a - b
+print(a, "-" , b, "=", result)
+result = a * b
+print(a, "*", b, "=", result)
+result = a / b
+print(a, "/", b, "=", result)
+
+os.system("Pause")
---
+++ day01/pythonCode02.py
@@ -0,0 +1,15 @@
+import turtle
+
+turtle.shape('turtle')
+
+#앞으로 이동
+turtle.forward(200)
+#오른쪽으로 90도 회전
+turtle.right(90)
+turtle.forward(200)
+turtle.right(90)
+turtle.forward(200)
+turtle.right(90)
+turtle.forward(200)
+
+turtle.done()
